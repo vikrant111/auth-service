72daa62555eb1b6d824c22a2f9be7fc823e1a4f8
Shreyash Dubey <shreyashlrn@gmail.com>
2025-05-02 22:53:54 +0530

    added error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,30 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./config/logger";
+import createHttpError, { HttpError } from "http-errors";
+import path from "path";
 const app = express();
 
 
 app.get("/", (req, res)=>{
      res.send("welcome to authn service")
+})
+
+
+
+//adding a global error handler middleware should always be kept at last
+app.use((err:HttpError, req: Request, res: Response, next: NextFunction)=>{
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        errors:[
+            {
+                type: err.name,
+                msg: err.message,
+                path: "",
+                location: ""
+            }
+        ]
+    })
 })
 
 
