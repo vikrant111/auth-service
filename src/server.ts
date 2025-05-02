131e0945ef909474c1a272e4c61c801c07bc5b7c
Shreyash Dubey <shreyashlrn@gmail.com>
2025-05-02 22:35:15 +0530

    added logger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/logger";
 
 
 const startServer = () => {
     try{
-       const port = Config.PORT 
-       app.listen(port, ()=>{
-        console.log("Listening on PORT", port)
+       const PORT = Config.PORT 
+       app.listen(PORT, ()=>{
+        logger.error("this is error log...")
+        logger.info("Server listening on port... ", { port : PORT })
     })
     }catch(err){
         console.log("Error while starting server", err)
